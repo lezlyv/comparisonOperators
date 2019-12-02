d858390d7a37a20c6e638908c28263f280faa468
lezlyv <57326531+lezlyv@users.noreply.github.com>
2019-12-01 20:31:10 -0600

    Initial commit

---
+++ practiceComparisonOperators.py
@@ -0,0 +1,61 @@
+'''
+Created on Nov 30, 2019
+
+@author: ITAUser
+'''
+
+4 == 4
+#True
+
+var = 4 == 4
+
+print(var) 
+
+a = 2 == 4
+
+print(a) 
+
+b = 2 < 3
+
+print(b) 
+
+c = 4 < 3
+
+print(c) 
+
+d = 4 > 3
+
+print(d) 
+
+e = 2 > 3
+
+print (e) 
+
+a = 2 <= 3
+
+print(a) 
+
+b = 2 >= 3 
+
+print(b) 
+
+print(e == b) 
+
+r = e == b 
+
+print(r) 
+
+string1 = "michael"
+
+string2 = "Michael"
+
+print(string1 == string2)
+
+a = [1, 2, 3] == [1, 2, 3] 
+
+print(a) 
+#true
+
+
+
+
